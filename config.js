cb2cb74cf7d41bb9f3fd33f73898d328de95d07f
insanexmaria <insanexpan@gmail.com>
2025-08-08 12:46:20 +0100

    config.js

--- config.js
+++ config.js
@@ -11,7 +11,7 @@ module.exports = {
   commandsDir: './commands',  
   embedColor: "#1db954",
   activityName: "YouTube Music", 
-  activityType: "LISTENING",  // Available activity types : LISTENING , PLAYING
+  activityType: "Listening",  // Available activity types : LISTENING , PLAYING
   SupportServer: "https://discord.gg/xQF9f9yUEM",
   embedTimeout: 5, 
   errorLog: "", 
